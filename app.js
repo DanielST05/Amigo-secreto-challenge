d6ad90cf321de99bd4470742c1e3f4fb1327bb01
DanielST05 <danielstgonzalezt@gmail.com>
2025-03-03 18:25:23 -0500

    Función para agregar un amigo a la lista

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombreAmigo = input.value.trim();
+
+    if (nombreAmigo === '') {
+        alert('Por favor, ingresa un nombre válido.');
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    input.value = ''; 
+
+    mostrarListaAmigos();
+}
